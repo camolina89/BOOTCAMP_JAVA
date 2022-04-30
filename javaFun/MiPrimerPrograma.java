cc0d4027774b4b0c3afd8843b607322d1d88dc2e
camolina89 <cristobal.molina.all@gmail.com>
2022-04-30 11:47:52 -0400

    MiPrimerPrograma

---
+++ javaFun/MiPrimerPrograma.java
@@ -0,0 +1,5 @@
+public class MiPrimerPrograma {
+	public static void main (String[] arg) {
+		System.out.println("\nMi nombre es Cristóbal Molina  \nTengo 32 años de edad \nMi ciudad es Chillan Viejo, CHI");	
+	}
+}
